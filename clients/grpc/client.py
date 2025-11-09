076f447052728cc2351d110165616878e38d98e3
Danil <danilgolovin28@yandex.ru>
2025-11-09 21:59:02 +0500

    Знакомство с gRPC API клиентами

---
+++ clients/grpc/client.py
@@ -0,0 +1,11 @@
+import grpc.experimental.gevent as grpc_gevent
+
+from grpc import Channel
+
+grpc_gevent.init_gevent()
+
+class GRPCClient:
+    def __init__(self, channel: Channel):
+        self.channel = channel
+
+
